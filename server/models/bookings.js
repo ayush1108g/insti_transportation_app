87c7536eb36c9bb4d86b753f68d78abd32e6eb32
Rachit Jain <rgjain04@gmail.com>
2024-03-30 10:39:41 +0530

    added Other option instead of UPI

--- server/models/bookings.js
+++ server/models/bookings.js
@@ -17,7 +17,7 @@ const bookingSchema = new mongoose.Schema({
   },
   paymentMethod: {
     type: String,
-    enum: ['UPI', 'payLater'],
+    enum: ['Other', 'payLater'],
     required: true
   },
 }, { timestamps: true });
