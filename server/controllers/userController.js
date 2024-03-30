4195b1def1c801fc1885825cea7b79892bf4d147
Rachit Jain <rgjain04@gmail.com>
2024-03-30 10:21:17 +0530

    added user Login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,26 @@
 const User = require('../models/user');
 
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (password !== user.password) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+router.post('/login', userController.login);
+
 
 router.get('/', userController.getAllUsers);
 
